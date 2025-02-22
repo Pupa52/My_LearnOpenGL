15b2a8c7b660f44cfa22e75edde40effca77c24a
Hyunwoo Shim <hkhunk0202@icloud.com>
2025-02-22 15:54:19 +0900

    Add Buffer(VBO, EBO)

--- src/context.cpp
+++ src/context.cpp
@@ -10,21 +10,31 @@ ContextUPtr Context::Create() {
 }
 bool Context::Init() {
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f, // top right
+        0.5f, -0.5f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f, // top left
+      };
+      uint32_t indices[] = { // note that we start from 0!
+        0, 1, 3, // first triangle
+        1, 2, 3, // second triangle
       };
 
     glGenVertexArrays(1, &m_vertexArrayObject);
     glBindVertexArray(m_vertexArrayObject);
 
-    glGenBuffers(1, &m_vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 9, vertices,
-        GL_STATIC_DRAW);
+    // Vertex Buffer Object
+    m_vertexBuffer = Buffer::CreateWithData(
+        GL_ARRAY_BUFFER, GL_STATIC_DRAW,
+        vertices, sizeof(float) * 12);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+
+    // Element Buffer Object
+	m_indexBuffer = Buffer::CreateWithData(
+        GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW,
+        indices, sizeof(uint32_t) * 6);
 
     ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
     ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
@@ -45,6 +55,6 @@ bool Context::Init() {
 void Context::Render() {
     glClear(GL_COLOR_BUFFER_BIT);
 
-  glUseProgram(m_program->Get());
-  glDrawArrays(GL_TRIANGLES, 0, 3);
+    m_program->Use();
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
