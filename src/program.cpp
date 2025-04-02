569c161f69ab75f115def00d67d6950935dfcd90
Pupa52 <hkhunk0202@icloud.com>
2025-04-02 21:59:36 +0900

    라이트맵 추가(Diffuse, Specular Texture)

--- src/program.cpp
+++ src/program.cpp
@@ -6,6 +6,17 @@ ProgramUPtr Program::Create(const std::vector<ShaderPtr>& shaders) {
       return nullptr;
     return std::move(program);
 }
+ProgramUPtr Program::Create(const std::string& vertShaderFilename,
+    const std::string& fragShaderFilename) 
+{
+    ShaderPtr vs = Shader::CreateFromFile(vertShaderFilename, GL_VERTEX_SHADER);
+    ShaderPtr fs = Shader::CreateFromFile(fragShaderFilename, GL_FRAGMENT_SHADER);
+
+    if (!vs || !fs)
+      return nullptr;
+
+    return std::move(Create({vs, fs}));
+}
 
 bool Program::Link(const std::vector<ShaderPtr>& shaders) {
     m_program = glCreateProgram();
@@ -37,19 +48,16 @@ void Program::Use() const
     glUseProgram(m_program);
 }
 
-// program.cpp
 void Program::SetUniform(const std::string& name, int value) const {
     auto loc = glGetUniformLocation(m_program, name.c_str());
     glUniform1i(loc, value);
 }
 
-void Program::SetUniform(const std::string& name,
-    const glm::mat4& value) const {
+void Program::SetUniform(const std::string& name, const glm::mat4& value) const {
     auto loc = glGetUniformLocation(m_program, name.c_str());
     glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(value));
 }
 
-// program.cpp
 void Program::SetUniform(const std::string& name, float value) const {
     auto loc = glGetUniformLocation(m_program, name.c_str());
     glUniform1f(loc, value);
@@ -58,4 +66,9 @@ void Program::SetUniform(const std::string& name, float value) const {
 void Program::SetUniform(const std::string& name, const glm::vec3& value) const {
     auto loc = glGetUniformLocation(m_program, name.c_str());
     glUniform3fv(loc, 1, glm::value_ptr(value));
+}
+
+void Program::SetUniform(const std::string& name, const glm::vec4& value) const {
+    auto loc = glGetUniformLocation(m_program, name.c_str());
+    glUniform4fv(loc, 1, glm::value_ptr(value));
 }
